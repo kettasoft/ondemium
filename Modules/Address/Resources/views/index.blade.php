0c6fc13490e3bbe892a4d2a8e1630ec479d78733
kettasoft <kettasoft@gmail.com>
2023-01-07 01:25:42 +0200

    Basic address module

---
+++ Modules/Address/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('address::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('address.name') !!}
+    </p>
+@endsection
