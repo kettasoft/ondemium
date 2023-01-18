2b8500459499e85d4b043d7ef9d36be0319fb26a
kettasoft <kettasoft@gmail.com>
2023-01-18 20:27:51 +0200

    Simple way to return a simple message after creation or update

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Arr;
 
 if (!function_exists('permission')) {
@@ -22,5 +23,22 @@ if (!function_exists('create_personal_access_token')) {
 	function create_personal_access_token($instance, $name, $abilities = [])
 	{
 		return $instance->createToken($name, $abilities)->plainTextToken;
+	}
+}
+
+if (! function_exists('alert')) {
+	/**
+	 * Simple way to return a simple message after creation or update
+	 * @param $message string
+	 * @param $success bool 
+	 * @param $status  int
+	 * @return \Illuminate\Http\Response
+	 */
+	function alert($message, $success = true, $status = 200): JsonResponse
+	{
+		return response()->json([
+			'success' => $success,
+			'message' => $message,
+		], $status);
 	}
 }
